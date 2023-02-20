6b0241d337eb0f60ba5a856fd554090b9c7c268f
Ahmad Azwar Annas <ahmadazw2@gmail.com>
2023-02-20 12:11:11 +0700

    Function to change camera ratio and camera type

--- App.tsx
+++ App.tsx
@@ -1,14 +1,28 @@
-import { Camera } from "expo-camera";
+import { Camera, CameraType } from "expo-camera";
 import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
-
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
 export default function App() {
+  const [cameraType, setCameraType] = useState(CameraType.back);
   const [cameraPermission, setCameraPermission] = useState(null);
+  const [ratio, setRatio] = useState("4:3");
+  const [containerRatio, setContainerRatio] = useState(3 / 4);
 
   const requestPermission = async () => {
     const permission = await Camera.requestCameraPermissionsAsync();
     setCameraPermission(permission.status === "granted");
+  };
+
+  const changeRatio = async () => {
+    setRatio(ratio === "4:3" ? "16:9" : "4:3");
+    setContainerRatio(containerRatio === 3 / 4 ? 9 / 16 : 3 / 4);
+  };
+
+  const changeCameraType = async () => {
+    setCameraType(
+      cameraType == CameraType.back ? CameraType.front : CameraType.back
+    );
   };
 
   useEffect(() => {
@@ -30,8 +44,88 @@ export default function App() {
   } else {
     return (
       <View style={styles.container}>
-        <Text>Keijo</Text>
-        <StatusBar style="auto" />
+        <View style={{ aspectRatio: containerRatio }}>
+          <Camera type={cameraType} style={{ flex: 1 }} ratio={ratio}>
+            <View
+              style={{
+                flex: 1,
+                width: "100%",
+                backgroundColor: "transparent",
+                flexDirection: "row",
+              }}
+            >
+              <View
+                style={{
+                  position: "absolute",
+                  left: "5%",
+                  top: "10%",
+                  flexDirection: "column",
+                  justifyContent: "space-between",
+                }}
+              >
+                <TouchableOpacity
+                  onPress={() => changeRatio()} // change this to a dedicated button after the settings menu is done
+                  style={{
+                    backgroundColor: "transparent",
+                    height: 24,
+                    width: 24,
+                  }}
+                >
+                  <FontAwesome5 name="cog" size={24} color="#cdd6f4" />
+                </TouchableOpacity>
+              </View>
+            </View>
+          </Camera>
+        </View>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: "#1e1e2e",
+            alignContent: "center",
+            justifyContent: "space-between",
+            alignItems: "center",
+            flexDirection: "row",
+            padding: 20,
+          }}
+        >
+          <TouchableOpacity
+            onPress={() => changeCameraType()}
+            style={{
+              width: 70,
+              height: 70,
+              borderRadius: 50,
+              backgroundColor: "transparent",
+              alignItems: "center",
+              alignContent: "center",
+              justifyContent: "center",
+              borderColor: "#b4befe",
+              borderWidth: 2,
+            }}
+          >
+            <FontAwesome5 name="sync" size={36} color="#cdd6f4" />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => console.log("Capture Image")}
+            style={{
+              width: 80,
+              height: 80,
+              borderRadius: 50,
+              backgroundColor: "#cdd6f4",
+              borderColor: "#b4befe",
+              borderWidth: 4,
+            }}
+          />
+          <TouchableOpacity
+            onPress={() => console.log("Open Image Preview")}
+            style={{
+              width: 70,
+              height: 70,
+              borderRadius: 50,
+              backgroundColor: "#cdd6f4",
+            }}
+          />
+        </View>
+        <StatusBar style="light" />
       </View>
     );
   }
@@ -46,8 +140,5 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
