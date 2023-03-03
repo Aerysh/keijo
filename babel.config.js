873a988eba4ff976c923e62c5760b566a4c201bd
Ahmad Azwar Annas <ahmadazw2@gmail.com>
2023-03-03 10:44:05 +0700

    Remove some unused dependencies

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,6 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo"],
     plugins: [
-      ["styled-components", { ssr: true }],
       "react-native-reanimated/plugin",
     ],
   };
